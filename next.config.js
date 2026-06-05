dcb92f334b0807a59b5701409201dd88e0a36ae1
Boluwatife Olaniyan <42306524+devkijified@users.noreply.github.com>
2026-06-05 23:29:50 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['image.tmdb.org', 'lh3.googleusercontent.com'],
+    domains: [
+      'image.tmdb.org',
+      'lh3.googleusercontent.com',
+      'api.dicebear.com',
+      'cdn-images.dzcdn.net',  // Deezer CDN for album covers
+      'e-cdns-images.dzcdn.net', // Alternative Deezer CDN
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
   },
 }
+
 module.exports = nextConfig
